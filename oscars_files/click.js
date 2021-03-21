76ef103609d3b0b659c6c748d880ab10bf99e2fb
atogt <81076280+atogt@users.noreply.github.com>
2021-03-21 19:59:12 +0100

    tst

---
+++ oscars_files/click.js
@@ -0,0 +1,12 @@
+function addClass() {
+  var a = document.getElementById("hiddenWinners");
+
+  if (a.classList.contains('active')) {
+  	a.classList.remove('active')
+
+  } else{
+  	a.classList.add('active');
+  }
+  	
+    
+}
